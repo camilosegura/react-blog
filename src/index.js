4dbd3dac60457ae5009a189557b931aa3989aae8
Camilo Segura <camilosegurar@gmail.com>
2017-11-01 11:35:29 -0500

    feat: added redux and react-redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
     <BrowserRouter>
+      <Provider>
         <App />
+      </Provider>
     </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
