357ad51a540e190c472727c232368562149aa1ec
Camilo Segura <camilosegurar@gmail.com>
2017-11-21 20:13:12 -0500

    feat: Controlled Comment form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Modal = ({ id, title, children }) => (
-    <div class="modal fade" id={id} tabindex="-1" role="dialog" aria-labelledby="title" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="title">{title}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <div className="modal fade" id={id} tabIndex="-1" role="dialog" aria-labelledby="title" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="title">{title}</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
+                <div className="modal-body">
                     { children }
                 </div>
             </div>
--- src/components/Post.js
+++ src/components/Post.js
@@ -11,11 +11,29 @@ class Post extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      author: '',
+      body: ''
+    };
     this.addComment = this.addComment.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   addComment() {
     window.$(`#${ID_MODAL}`).modal('show');
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+    console.log(this.state)
+  }
+
+  onChange() {
+    this.setState({
+      author: this.author.value,
+      body: this.body.value
+    })
   }
 
   componentDidMount() {
@@ -28,30 +46,53 @@ class Post extends Component {
     return (
       <div className="post">
         <Card document={post} addComment={this.addComment}>
-          { comments.map(comment => (
+          {comments.map(comment => (
             <Card document={comment} key={comment.id} />
-          )) }
+          ))}
         </Card>
         <Modal id={ID_MODAL}>
-           
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="author">Author</label>
+              <input 
+                type="text" 
+                className="form-control" 
+                name="author" 
+                placeholder="Author" 
+                ref={author => { this.author = author}}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="body">Body</label>
+              <textarea 
+                className="form-control" 
+                name="body" 
+                rows="3" 
+                ref={body => { this.body = body }}
+                onChange={this.onChange}
+              ></textarea>
+            </div>
+            <button className="btn btn-success" type="submit">Send</button>
+          </form>
         </Modal>
       </div>
-    )
+        )
   }
 }
 
 const mapStateToProps = (state, ownState) => {
   const posts = state.posts.filter(post => post.id === ownState.match.params.id);
   const post = posts.length && posts[0];
-  
+
   return {
-    post,
-    comments: state.comments.filter(comment => comment.parentId === ownState.match.params.id)
+          post,
+        comments: state.comments.filter(comment => comment.parentId === ownState.match.params.id)
   }
 };
 
 const mapDispatchToProps = dispatch => ({
-  getComments: id => dispatch(fetchComments(id))
+          getComments: id => dispatch(fetchComments(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Post);
